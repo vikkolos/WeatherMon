125d2adb9c4adf726b449ec4cb5f2d4af51e99a0
Vikas <vikas27058@gmail.com>
2025-10-29 01:21:42 +0530

    "add more ui "

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -2,37 +2,63 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 import { IoSearch } from "react-icons/io5";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
-  const [places,setPlaces] = useState([])
+  const [places, setPlaces] = useState([]);
+  const [location, setLocation] = useState(null);
+
+  const getCurrentLoc = function () {
+    if (!navigator.geolocation) {
+      console.error("Your brwser doset support geolocation");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
+      },
+      (error) => {
+        console.error("Your brwser doset support geolocation");
+        alert("cant find location enter manually ");
+      }
+    );
+  };
 
   useEffect(() => {
-    // don't run if search is too short
+    getCurrentLoc();
+  }, []);
+
+  useEffect(() => {
     if (searchValue.length < 2) return;
-  
+
     const timeout = setTimeout(async () => {
       try {
         const response = await axios.get(
           `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${searchValue}`,
           {
             headers: {
-              "X-RapidAPI-Key": "0b07b22316mshbce3fcfd8d07384p19c366jsnd29201ce6597",
+              "X-RapidAPI-Key":
+                "0b07b22316mshbce3fcfd8d07384p19c366jsnd29201ce6597",
               "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
             },
           }
         );
         console.log(response.data.data);
+        console.log(response.data);
         setPlaces(response.data.data);
       } catch (error) {
         console.error(error);
       }
     }, 400); // wait 400ms after typing
-  
     // cleanup function to cancel old timers
     return () => clearTimeout(timeout);
   }, [searchValue]);
-    
+
   return (
     <>
       <div className="min-h-screen w-full bg-white relative max-w-[1442px] mx-auto">
@@ -53,16 +79,62 @@ function App() {
         <div className=" w-60 h-20 absolute ml-20 z-11 bg-white border-black border-2 mt-20 rounded-lg text-black flex items-center justify-center">
           <p className=" text-3xl font-extrabold inset-0 mb-1">WeatherMon</p>
         </div>
+
         <div className=" w-230 h-[65.3vh] bg-[#0100f2] absolute z-10 ml-50 mt-30 text-amber-50 text-6xl ">
-          
+          <div className="bg-white w-[60%] h-[54%] left-9 absolute top-15 rounded-lg">
+            
+          </div>
+          <div className="bg-white w-[60%] h-[25%] left-9 absolute top-[68.8%] rounded-lg">
+            
+          </div>
+          <div className="bg-white w-[30%] h-[25%] left-[64%] ml-3.5 absolute top-15 rounded-lg">
+
+          </div>
+          <div className="bg-white w-[30%] h-[25%] left-[64%] ml-3.5 absolute top-[40%] rounded-lg">
+
+          </div>
+          <div className="bg-white w-[30%] h-[25%] left-[64%] ml-3.5 absolute top-[68.8%] rounded-lg">
+
+          </div>
         </div>
+
         <div className="w-lg h-15 absolute ml-102 z-11 bg-white border-black border-2 mt-22.5 rounded-lg text-black flex items-center justify-center ">
-          <input type="text" placeholder="Search the place" className="w-full rounded-2xl p-1 m-3 px-4 focus:outline-none focus:ring-2 focus:ring-[#0100f2] transition duration-150 ease-in-out" value={searchValue} onChange={(e)=>setSearchValue(e.target.value)}/>
-          <button className="w-20 h-full border-l-2 items-center flex justify-center pt-0.5"><IoSearch size={30} /></button>
+          <input
+            type="text"
+            placeholder="Search the place"
+            className="w-full rounded-2xl p-1 m-3 px-4 focus:outline-none focus:ring-2 focus:ring-[#0100f2] transition duration-150 ease-in-out"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <button className="w-20 h-full border-l-2 items-center flex justify-center pt-0.5">
+            <IoSearch size={30} />
+          </button>
         </div>
-            {/* <div className="w-lg h-15 absolute ml-102 z-11 bg-white border-black border-2 mt-38 rounded-lg text-black flex items-center justify-center ">
-         
-           </div> */}
+        {places.length > 0 && (
+          <div className="w-lg min-h-15 absolute ml-102 z-11 bg-white border-black border-2 mt-38 rounded-lg text-black flex items-center justify-center overflow-auto ">
+            <ul className="w-full rounded-lg">
+              {places.map((place) => (
+                <li
+                  className="p-3 border-1"
+                  key={place.id}
+                  onClick={() => {
+                    setPlaces([]);
+                    setSearchValue(place.city);
+                  }}
+                >
+                  {place.city},{place.country}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <div className="w-auto h-20 absolute left-[70%] z-11 bg-white border-black border-2 top-20 rounded-lg text-black flex items-center justify-evenly gap-1">
+        <div className="p-2 ml-2">
+          <MdOutlineLocationOn size={25} />
+        </div>
+        <h3 className="p-2 mr-2 font-bold text-lg">Get Current Location</h3>
+        </div>
       </div>
     </>
   );
